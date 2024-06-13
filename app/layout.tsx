fb12bf3e2f2b67c5e7ea1e4d6059128f80b0cb93
nexwan <lcontrerasmartinez235@gmail.com>
2024-06-13 10:31:28 -0600

    xd

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "./components/Header";
-import Navbar from "./components/Navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
