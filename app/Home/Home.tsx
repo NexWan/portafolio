8eb0c584d260546b5003511f149dbd296b4428f5
nexwan <lcontrerasmartinez235@gmail.com>
2024-06-13 10:14:38 -0600

    man

--- app/Home/Home.tsx
+++ app/Home/Home.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import styles from './home.module.css'
 
-const Home = () => {
+const home = () => {
     let name:string = "John Doe";
   return (
     <div id='homeBody' className={`${styles.homeBody} ${' text-2xl rounded-b-xl px-5 h-screen'}`}>
@@ -10,4 +10,4 @@ const Home = () => {
   )
 }
 
-export default Home
+export default home
